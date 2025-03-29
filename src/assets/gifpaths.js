905fd27d240ab1050694150fcff97053ce8de43e
barada02 <chandanbarada2@gmail.com>
2025-03-29 20:39:43 +0530

    asset configured

---
+++ src/assets/gifpaths.js
@@ -0,0 +1,4 @@
+
+const ariana ="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExZmgwdHByOWxodHI2OHV1b2Zsb2gwNjFnb202bTY4ZDVtYWNqcWF3dSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/XFR9iUi7RpC41XsAGD/giphy.gif"
+const anime= "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExYzVtODNzMzltZnUyMHhicTdkZWdhMjM2aDZocDczZDhueWJnajdqaSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/PAvpExxgM9f0rypv96/giphy.gif"
+export { ariana, anime }
